4e10ca1f2d68e101e5f32e2a5ee7661a532179f2
1k_mile <kamafeed1212@gmail.com>
2024-03-10 23:54:55 +0300

    create and setup project with pods

---
+++ AnimeWave/AnimeWave/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AnimeWave
+//
+//  Created by Камил Хайрутдинов on 10.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
