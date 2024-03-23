d8668ca805b94d82e0d7e03739f2ef8ebbc5343e
1k_mile <kamafeed1212@gmail.com>
2024-03-23 15:34:31 +0300

    spaces refactor

--- AnimeWave/AnimeWave/Helpers/LayoutConstant.swift
+++ AnimeWave/AnimeWave/Helpers/LayoutConstant.swift
@@ -8,79 +8,78 @@
 
 import UIKit
 
- enum LayoutConstants {
+enum LayoutConstants {
 
-     // MARK: - AuthViewController
-     enum AuthView {
-         enum LogoImageView {
-             static let topOffsetMultiplier: CGFloat = 1.0 / 3.0
-         }
-         enum SignInButton {
-             static let bottomOffset: CGFloat = 70
-         }
-         enum SignUpButton {
-             static let topOffset: CGFloat = 25
-         }
-     }
+    // MARK: - AuthViewController
+    enum AuthView {
+        enum LogoImageView {
+            static let topOffsetMultiplier: CGFloat = 1.0 / 3.0
+        }
+        enum SignInButton {
+            static let bottomOffset: CGFloat = 70
+        }
+        enum SignUpButton {
+            static let topOffset: CGFloat = 25
+        }
+    }
 
-     // MARK: - SignUpViewController
-     enum SignUpView {
-         enum StackView {
-             static let horizontalInset: CGFloat = 50
-         }
+    // MARK: - SignUpViewController
+    enum SignUpView {
+        enum StackView {
+            static let horizontalInset: CGFloat = 50
+        }
 
-         enum ValidationErrorsLabel {
-             static let topOffset: CGFloat = 10
-         }
+        enum ValidationErrorsLabel {
+            static let topOffset: CGFloat = 10
+        }
 
-         enum NextButton {
-             static let bottomOffset: CGFloat = 50
-         }
-     }
+        enum NextButton {
+            static let bottomOffset: CGFloat = 50
+        }
+    }
 
-     // MARK: - ConfigureProfileViewController
-     enum ConfigureProfileView {
-         enum FillProfileLabel {
-             static let topOffset: CGFloat = 70
-         }
+    // MARK: - ConfigureProfileViewController
+    enum ConfigureProfileView {
+        enum FillProfileLabel {
+            static let topOffset: CGFloat = 70
+        }
 
-         enum UserAvatarImageView {
-             static let topOffset: CGFloat = 100
-             static let widthHeightMultiplier: CGFloat = 0.5
-         }
+        enum UserAvatarImageView {
+            static let topOffset: CGFloat = 100
+            static let widthHeightMultiplier: CGFloat = 0.5
+        }
 
-         enum PickImageButton {
-             static let topOffset: CGFloat = 10
-         }
+        enum PickImageButton {
+            static let topOffset: CGFloat = 10
+        }
 
-         enum LoginTextField {
-             static let topOffset: CGFloat = 30
-             static let horizontalInset: CGFloat = 50
-         }
+        enum LoginTextField {
+            static let topOffset: CGFloat = 30
+            static let horizontalInset: CGFloat = 50
+        }
 
-         enum SignUpButton {
-             static let bottomOffset: CGFloat = 50
-         }
-     }
+        enum SignUpButton {
+            static let bottomOffset: CGFloat = 50
+        }
+    }
 
-     // MARK: - SignInViewController
-     enum SignInView {
-         enum StackView {
-             static let horizontalInset: CGFloat = 50
-         }
+    // MARK: - SignInViewController
+    enum SignInView {
+        enum StackView {
+            static let horizontalInset: CGFloat = 50
+        }
 
-         enum ValidationErrorsLabel {
-             static let topOffset: CGFloat = 10
-         }
+        enum ValidationErrorsLabel {
+            static let topOffset: CGFloat = 10
+        }
 
-         enum SignInButton {
-             static let bottomOffset: CGFloat = 50
-         }
-     }
+        enum SignInButton {
+            static let bottomOffset: CGFloat = 50
+        }
+    }
 
-     // MARK: - Global
-     static let buttonsHeight: CGFloat = 40
-     static let textFieldsheight: CGFloat = 40
- }
-
+    // MARK: - Global
+    static let buttonsHeight: CGFloat = 40
+    static let textFieldsheight: CGFloat = 40
+}
 // swiftlint:enable all
